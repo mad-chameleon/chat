5d46c4adecaf6585470cabbde12fdaaa0f2edff0
svetlana <svetlanaa.anisimova@yandex.ru>
2024-07-02 01:25:58 +0300

    refactored code

--- frontend/src/components/MessagesList.jsx
+++ frontend/src/components/MessagesList.jsx
@@ -27,7 +27,7 @@ const MessagesList = () => {
       <div className="bg-light mb-4 p-3 shadow-sm small">
         <p className="m-0">
           <b>
-            {channels.length > 0 && currentChannel.name && `# ${currentChannel.name}`}
+            {channels.length > 0 && currentChannel && currentChannel.name && `# ${currentChannel.name}`}
           </b>
         </p>
         <span className="text-muted">{t('form.messages.messagesCount.count', { count: messagesCount })}</span>
